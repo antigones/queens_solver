96f338e5d8c894dd28771f7e0318b507bcc9cb2e
antigones <juna.salviati@gmail.com>
2025-01-25 10:02:02 +0100

    add missing check

--- queens_solver.py
+++ queens_solver.py
@@ -27,7 +27,8 @@ class QueensSolver:
     def place_queen(self, col, placed_queens_positions):
         self.moves.append(placed_queens_positions)
          
-        if col >= self.nr_of_queens and all(len(lst) > 0 for lst in placed_queens_positions.values()):
+        if col >= self.nr_of_queens and all(len(lst) == 1 for lst in placed_queens_positions.values()):
+            print('placed_queens_positions',placed_queens_positions)
             return True
         
         for i in range(self.nr_of_queens):
